3762729770967a4c1011756e34c1f5c27011c4b0
sapanadhakal40 <sapna.dhakal40@gmail.com>
2024-10-16 19:48:15 +0300

    Add initial script.js file

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLink = document.querySelector('.nav-link');
+    const navIcon = navToggle.querySelector('i');
+    const dropdown = document.querySelector('.dropdown');
+    const dropdownContent = document.querySelector('.dropdown-content');
+    const homeLink = document.getElementById('home-link');
+
+    navToggle.addEventListener('click', () => {
+        navLink.classList.toggle('active');
+        navToggle.classList.toggle('active');
+
+
+        if (navToggle.classList.contains('active')) {
+            navIcon.classList.remove('fa-bars');
+            navIcon.classList.add('fa-times');
+        } else {
+            navIcon.classList.remove('fa-times');
+            navIcon.classList.add('fa-bars');
+        }
+    });
+
+    homeLink.addEventListener('mouseover', function () {
+        console.log('hovered on home')
+        homeLink.style.setProperty('background-color', 'red', 'important');
+    });
+    homeLink.addEventListener('mouseout', function () {
+        console.log('Not hovering on Home');
+        homeLink.style.removeProperty('background-color'); // Reset the color back to default when not hovering
+    });
+
+
+
+
+});
